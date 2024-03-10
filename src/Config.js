87d04872fde01388b8282481b193adbc221c3989
alejozd <alejozd79@gmail.com>
2024-03-10 18:09:46 -0500

    url aws

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,7 @@
 // Config.js
 const Config = {
-    apiUrl: "http://localhost:8080" // Local dev
-    //apiUrl: "https://quoterb.onrender.com" // Prod url
+    //apiUrl: "http://localhost:8080" // Local dev
+    apiUrl: "http://ec2-34-235-143-188.compute-1.amazonaws.com" // Prod url aws
 };
 
 export default Config;
