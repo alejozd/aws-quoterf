439b223900442f255176b2351d79f116659a385d
Alejandro Zambrano <alejozd79@gmail.com>
2024-03-14 14:06:31 -0500

    faltaba el http

--- src/Config.js
+++ src/Config.js
@@ -2,7 +2,7 @@
 const Config = {
     //apiUrl: "http://localhost:8080" // Local dev
     //apiUrl: "http://ec2-34-235-143-188.compute-1.amazonaws.com:8080" // Prod url aws ntes de la elastica
-    apiUrl: "ec2-3-212-29-182.compute-1.amazonaws.com:8080" // Prod url aws con elastic IP    
+    apiUrl: "http://ec2-3-212-29-182.compute-1.amazonaws.com:8080" // Prod url aws con elastic IP    
     //apiUrl: "34.235.143.188:8080" // Prod url aws
 };
 
