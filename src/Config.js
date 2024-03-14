cc8dd229232dabd51531800e823c4faec8ceceb0
Alejandro Zambrano <alejozd79@gmail.com>
2024-03-14 13:55:17 -0500

    nueva ip elastica

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,8 @@
 // Config.js
 const Config = {
     //apiUrl: "http://localhost:8080" // Local dev
-    apiUrl: "http://ec2-34-235-143-188.compute-1.amazonaws.com:8080" // Prod url aws
+    //apiUrl: "http://ec2-34-235-143-188.compute-1.amazonaws.com:8080" // Prod url aws ntes de la elastica
+    apiUrl: "ec2-3-212-29-182.compute-1.amazonaws.com" // Prod url aws con elastic IP    
     //apiUrl: "34.235.143.188:8080" // Prod url aws
 };
 
